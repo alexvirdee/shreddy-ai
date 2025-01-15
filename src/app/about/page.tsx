eac2607ebfaa3d462368eb55d7da2f46b20e57bc
Alex Virdee <alex.virdee08@gmail.com>
2025-01-14 20:02:57 -0500

    resolve deploy failing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,7 @@ export default function About() {
             />
             </div>
             <div className="justify-center w-full md:w-1/2 mx-auto text-base mt-8 p-12">
-                Shreddy is an AI-powered guitar mentor designed to revolutionize the way guitarists learn and play. Combining cutting-edge technology with the soul of a true rockstar, Shreddy delivers personalized guidance, generates epic riffs, and offers real-time feedback to help players of all levels sharpen their skills. Whether you're mastering your first chord or perfecting your solos, Shreddy is here to make learning fun, engaging, and uniquely tailored to your style. Get ready to shred like never before with the ultimate AI guitar agent!
+                Shreddy is an AI-powered guitar mentor designed to revolutionize the way guitarists learn and play. Combining cutting-edge technology with the soul of a true rockstar, Shreddy delivers personalized guidance, generates epic riffs, and offers real-time feedback to help players of all levels sharpen their skills. Whether mastering your first chord or perfecting your solos, Shreddy is here to make learning fun, engaging, and uniquely tailored to your style. Get ready to shred like never before with the ultimate AI guitar agent!
             </div>
         </div>
     )
