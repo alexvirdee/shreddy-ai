e12449a111dddd135b4a64af3da1fe96befc6589
Alex Virdee <alex.virdee08@gmail.com>
2025-01-15 00:04:18 -0500

    feat: test chat api route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,26 @@
+import OpenAI from 'openai';
+
+const client = new OpenAI({
+    apiKey: process.env['OPENAI_API_SECRET'], // This is the default and can be omitted
+});
+
 export async function GET(request: Request) {
-    return new Response('Hello Shreddy!', {
-        status: 200,
-    });
+    try {
+        const chatCompletion = await client.chat.completions.create({
+            messages: [{ role: 'user', content: 'Say this is a test' }],
+            model: 'gpt-4o',
+        });
+
+        // Return the response from OpenAI
+        return new Response(JSON.stringify(chatCompletion), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        return new Response('Something went wrong!', {
+            status: 500,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    }
 }
