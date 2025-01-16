ed411dda66c610442326e2b56021a1db594873d9
Alex Virdee <alex.virdee08@gmail.com>
2025-01-16 15:15:12 -0500

    update twitter link since username change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://x.com/ShreddyA67473"
+          href="https://x.com/ShreddyAI"
           target="_blank"
           rel="noopener noreferrer"
         >
